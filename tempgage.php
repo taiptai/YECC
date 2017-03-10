bcaee48b7273f4b7dcf347db158fa78b6b01a909
taiptai <vacharapong.cw@gmail.com>
2017-03-11 03:33:18 +0700

    Update tempgage.php

--- tempgage.php
+++ tempgage.php
@@ -1,21 +1,41 @@
-  <html>
+<html>
   <head>
-  <script src="http://yecccmu-test.herokuapp.com/justgage.js"></script>
-  <script src="http://yecccmu-test.herokuapp.com/raphael-2.1.4.min.js"></script>
-  
-  <script>
-  var g = new JustGage({
-    id: "gauge",
-    value: 25,
-    min: 0,
-    max: 50,
-    title: "Temperature"
-  });
-</script>
+    <title>Auto-adjust</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style>
+      body {
+        text-align: center;
+      }
+
+      #g1 {
+        width:200px; height:200px;
+        display: inline-block;
+        margin: 1em;
+      }
+    </style>
+
   </head>
   <body>
+    <div id="g1"></div>
+	<script src="http://yecccmu-test.herokuapp.com/raphael-2.1.4.min.js"></script>
+    <script src="http://yecccmu-test.herokuapp.com/justgage.js"></script>
+    <script>
+      var g1;
 
-    <div id="gauge" style="width: 300px; height: 200px"></div>
+      window.onload = function(){
+        var g1 = new JustGage({
+          id: "g1",
+          value: getRandomInt(0, 50),
+          min: 0,
+          max: 50,
+          title: "Temperature",
+          label: "ºC"
+        });
 
+        setInterval(function() {
+          g1.refresh(getRandomInt(0, 50));
+        }, 1000);
+      };
+    </script>
   </body>
 </html>
