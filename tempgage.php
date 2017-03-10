e8f4215084c547d028d6e01c5c5dc03c7256e0fa
taiptai <vacharapong.cw@gmail.com>
2017-03-11 02:53:04 +0700

    Create tempgage.php

---
+++ tempgage.php
@@ -0,0 +1,21 @@
+  <html>
+  <head>
+  <script src="http//:yecccmu-test.herokuapp.com/raphael.2.1.0.min.js"></script>
+  <script src="http//:yecccmu-test.herokuapp.com/justgage.1.0.1.min.js"></script>
+  
+  <script>
+  var g = new JustGage({
+    id: "gauge",
+    value: 25,
+    min: 0,
+    max: 50,
+    title: "Visitors"
+  });
+</script>
+  </head>
+  <body>
+	  <center>
+    <div id="gauge" class="200x160px"></div>
+		</center>
+  </body>
+</html>
