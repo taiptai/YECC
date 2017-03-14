60a563fa7a103e4f9c47b777a48bc5bb937ef71a
taiptai <vacharapong.cw@gmail.com>
2017-03-15 04:47:32 +0700

    Update humidgage.php

--- humidgage.php
+++ humidgage.php
@@ -1,7 +1,11 @@
 <html>
-  <head>
-    <title>Auto-adjust</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <head>
+	
+        <meta charset="UTF-8">
+        <script src="https://netpie.io/public/netpieio/microgear.js"></script>
+		<script src="http://yecccmu-test.herokuapp.com/raphael-2.1.4.min.js"></script>
+		<script src="http://yecccmu-test.herokuapp.com/justgage.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <style>
       body {
         text-align: center;
@@ -13,27 +17,58 @@
         margin: 1em;
       }
     </style>
-
-  </head>
-  <body>
-    <div id="g1"></div>
-	<script src="http://yecccmu-test.herokuapp.com/raphael-2.1.4.min.js"></script>
-    <script src="http://yecccmu-test.herokuapp.com/justgage.js"></script>
+	<p hidden id="data"></p>
+    </head>
+    <body>
+        <center>
+         
+		 <div id="g1"></div>
+        </center>
     <script>
-      var g1;
+      const APPID     = 'SMART';
+      const APPKEY    = '8bQ3bAAw8pWOvxW';
+      const APPSECRET = 'J1xZ3Kaa3DZ0uHsVSOjqukv3F';
 
-      window.onload = function(){
-        var g1 = new JustGage({
-          id: "g1",
-          value: 40,
-          min: 0,
-          max: 100,
-          title: "Humidity",
-          label: "%"
+        var microgear = Microgear.create({
+            key: APPKEY,
+            secret: APPSECRET
         });
 
+        microgear.on('message',function(topic,msg) {
+           document.getElementById("data").innerHTML = msg;
+        });
 
-      };
-    </script>
-  </body>
+        microgear.on('present', function(event) {
+            console.log(event);
+        });
+
+        microgear.on('absent', function(event) {
+            console.log(event);
+        });
+		microgear.subscribe("/gearname/Smarthydro/temp")
+        microgear.connect(APPID);
+		
+		
+    var z1;
+    document.addEventListener("DOMContentLoaded", function(event) {
+        z1 = new JustGage({
+            id: "g1",
+			decimals: true,
+            value: 0,
+            min: 0,
+            max: 100,
+            title: "Humidity",
+            label: "%"
+        });
+	setInterval(function() {
+            z1.refresh(document.getElementById("data").innerHTML);
+        }, 1000);
+    });
+		</script>
+		
+
+    </body>
+
+	
+	
 </html>
